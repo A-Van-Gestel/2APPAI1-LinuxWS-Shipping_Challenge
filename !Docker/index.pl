4cf4a462aa833d8095675ce50cc98d92898e9058
Axel Van Gestel <r0784084@student.thomasmore.be>
2020-12-29 16:02:10 +0100

    Initial Commit

---
+++ !Docker/index.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+print "Content-type:text/html\r\n\r\n";
+print '<html>';
+print '<head>';
+print '<title>Hello World - First CGI script on Apache2!</title>';
+print '</head>';
+print '<body>';
+print '<h2>Hello World! TUX is in the house!</h2>';
+print '</body>';
+print '</html>';
