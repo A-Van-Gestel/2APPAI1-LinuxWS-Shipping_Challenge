2b5c2d7a5c1d8fca05a615a7b95561beb6502cac
Axel Van Gestel <r0784084@student.thomasmore.be>
2020-12-29 18:42:22 +0100

    Added: added server ip to index.pl

--- !Docker/index.pl
+++ !Docker/index.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+use Socket 'inet_ntoa';
+use Sys::Hostname 'hostname';
+my $addr = inet_ntoa(scalar gethostbyname(hostname() || 'localhost'));
+
+
 print "Content-type:text/html\r\n\r\n";
 print '<html>';
 print '<head>';
@@ -6,5 +11,6 @@ print '<title>Hello World - First CGI script on Apache2!</title>';
 print '</head>';
 print '<body>';
 print '<h2>Hello World! TUX is in the house!</h2>';
+print "<p>Server IP = $addr </p>";
 print '</body>';
 print '</html>';
