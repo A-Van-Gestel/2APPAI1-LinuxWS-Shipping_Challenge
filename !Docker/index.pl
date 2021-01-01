3890df5834370f6206cd86d5b19645313df838bf
Axel Van Gestel <r0784084@student.thomasmore.be>
2021-01-01 21:01:07 +0100

    Updated: index.pl

--- !Docker/index.pl
+++ !Docker/index.pl
@@ -26,7 +26,7 @@ print '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitt
 print '</head>';
 print '<body>';
 print '<h2>Hello World! TUX is in the house!</h2>';
-print '<p">Running MySQL + Perl + Apache2</p>';
+print '<p">Running MySQL + Apache2 + Perl</p>';
 print "<p>Server IP = $server_ip</p>";
 
 
