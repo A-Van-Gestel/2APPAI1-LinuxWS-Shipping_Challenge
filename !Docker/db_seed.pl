52414d5de6d4dece7afb38f8c575b708bea61a4d
Axel Van Gestel <r0784084@student.thomasmore.be>
2021-01-02 19:40:56 +0100

    Updated: index.pl, db_seed, db_update_name.pl

--- !Docker/db_seed.pl
+++ !Docker/db_seed.pl
@@ -27,7 +27,7 @@ Content-type:text/html\n\n
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css"/>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
         <a class="navbar-brand">Shipping Challenge</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
